d60d62697b2dd667798efd9be4fc00a2ad63dda9
galactrax <anthonycweathersby@gmail.com>
2020-03-28 08:06:20 -0600

    Update math files

---
+++ source/library/primitive/math/math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "primitive/math/line.h"
+#include "primitive/math/mat33.h"
+#include "primitive/math/mat44.h"
+#include "primitive/math/qbezier.h"
+#include "primitive/math/quat.h"
+#include "primitive/math/vec2.h"
+#include "primitive/math/vec3.h"
+#include "primitive/math/vec4.h"
+
