3acb35b805da62fa3576d82d2a97b4382ff9556e
galactrax <anthonycweathersby@gmail.com>
2020-03-28 14:46:20 -0600

    Update platformer.cpp

--- source/samples/platformer.cpp
+++ source/samples/platformer.cpp
@@ -9,21 +9,36 @@
 int main(int arg_count, char ** arguments)
 {
 
-    hive::interface::GLFWScreen screen(200, 200);
+    hive::interface::GLFWBoss boss(200, 200);
 
-    if (!screen.init()) return -1;
+    if (!boss.init()) return -1;
 
 
     hive::graphic::Quadrilateral box;
 
-    hive::interface::keyboard::Keyboard keyboard;
+    auto keyboard   = boss.getKeyboard();
+    auto controller = boss.getController(0);
+
+    if (!controller.isALIVE()) return -1;
 
     // making background color black as first
     // 3 arguments all are 0.0
     glClearColor(0.0, 0.0, 0.0, 1.0);
 
     while (true) {
-        screen.update();
+        controller.poll();
+        if (controller.hasChanged()) {
+            std::cout << controller << std::endl;
+        }
+
+
+        int dur = keyboard.getKeyPressDuration(hive::interface::KeyName::A);
+
+        if (dur > 0 && !keyboard.isKeyPressed(hive::interface::KeyName::A)) {
+            std::cout << "Key A was pressed for " << std::to_string(dur) << "ms" << std::endl;
+        }
+
+        boss.update();
         box.draw();
     }
 }
