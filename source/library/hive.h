ba19804bc311d31a4694154626b0fdde9cade15a
galactrax <anthonycweathersby@gmail.com>
2020-03-28 08:05:20 -0600

    Add hive root file

---
+++ source/library/hive.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+#define HIVE_USE_GLFW
+#define HIVE_DEBUG
+
+// Graphics
+#include "gpu/opengl/gl.h"
+
+#include "graphic/box.hpp"
+
+
+// Interfac
+#include "interface/drawing/screen.hpp"
+#include "interface/input/keyboard/keyboard.hpp"
+
+// Utility
+#include "primitive/log.h"
+
+
+// MATHS Stuff
+
+#include "primitive/math/line.h"
+#include "primitive/math/mat33.h"
+#include "primitive/math/mat44.h"
+#include "primitive/math/qbezier.h"
+#include "primitive/math/quat.h"
+#include "primitive/math/vec2.h"
+#include "primitive/math/vec3.h"
+#include "primitive/math/vec4.h"
